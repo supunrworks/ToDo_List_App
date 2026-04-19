f8f2e0b220659f32b9a719c5b1f3e069037ebebd
Supun Rashmika <supunrworks@gmail.com>
2026-04-20 00:23:20 +0530

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,37 @@
+const txttask = document.getElementById("txttask");
+const sub = document.getElementById("sub");
+const result = document.getElementById("results");
+
+sub.addEventListener("click", (e) => {
+    e.preventDefault()
+    const listitem = document.createElement("li");
+    const additem = document.createElement("span");
+    const delbtn = document.createElement("button");
+    const donebtn = document.createElement("button");
+
+    additem.classList.add("item");
+    additem.innerText = txttask.value;
+
+    delbtn.classList.add("del");
+    delbtn.innerText = "Delete";
+
+    donebtn.classList.add("done");
+    donebtn.innerText = "Done";
+
+
+    listitem.appendChild(additem);
+    listitem.appendChild(donebtn);
+    listitem.appendChild(delbtn);
+    result.appendChild(listitem);
+
+    delbtn.addEventListener("click", () => {
+        result.removeChild(listitem)
+    })
+
+    donebtn.addEventListener("click", () => {
+        const span = listitem.querySelector(".item");
+        span.style.color="gray";
+        span.style.textDecoration = "line-through";
+    })
+
+})
