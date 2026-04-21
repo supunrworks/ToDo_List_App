f5991217e22a1a6742e0e80a1e0e679ba94c3ac1
Supun Rashmika <supunrworks@gmail.com>
2026-04-21 10:06:37 +0530

    Did UX changes

--- script.js
+++ script.js
@@ -3,35 +3,42 @@ const sub = document.getElementById("sub");
 const result = document.getElementById("results");
 
 sub.addEventListener("click", (e) => {
-    e.preventDefault()
-    const listitem = document.createElement("li");
-    const additem = document.createElement("span");
-    const delbtn = document.createElement("button");
-    const donebtn = document.createElement("button");
+    if (txttask.value.length > 0) {
+        e.preventDefault()
+        const listitem = document.createElement("li");
+        const additem = document.createElement("span");
+        const delbtn = document.createElement("button");
+        const donebtn = document.createElement("button");
 
-    additem.classList.add("item");
-    additem.innerText = txttask.value;
+        additem.classList.add("item");
+        additem.innerText = txttask.value;
 
-    delbtn.classList.add("del");
-    delbtn.innerText = "Delete";
+        delbtn.classList.add("del");
+        delbtn.innerText = "Delete";
 
-    donebtn.classList.add("done");
-    donebtn.innerText = "Done";
+        donebtn.classList.add("done");
+        donebtn.innerText = "Done";
 
 
-    listitem.appendChild(additem);
-    listitem.appendChild(donebtn);
-    listitem.appendChild(delbtn);
-    result.appendChild(listitem);
+        listitem.appendChild(additem);
+        listitem.appendChild(donebtn);
+        listitem.appendChild(delbtn);
+        result.appendChild(listitem);
 
-    delbtn.addEventListener("click", () => {
-        result.removeChild(listitem)
-    })
+        delbtn.addEventListener("click", () => {
+            result.removeChild(listitem)
+        })
 
-    donebtn.addEventListener("click", () => {
-        const span = listitem.querySelector(".item");
-        span.style.color="gray";
-        span.style.textDecoration = "line-through";
-    })
+        donebtn.addEventListener("click", () => {
+            const span = listitem.querySelector(".item");
+            span.style.color = "gray";
+            span.style.textDecoration = "line-through";
+        })
+
+        txttask.value = ""
+        txttask.focus()
+    } else {
+        e.preventDefault()
+    }
 
 })
